b176fe93df93844c3f1e555166b25042c8420153
Olivier Fortin <ofortin@bnfsoftware.com>
2016-08-05 16:18:17 -0400

    fix

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+var imageop = require('gulp-image-optimization');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('default', function () {
+    gulp.src('dev/css/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('build/css'));
+
+    gulp.src('dev/img/**')
+        .pipe(imageop({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('img'));
+
+    gulp.src('dev/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+
+    gulp.src('dev/*.html')
+      .pipe(htmlmin({collapseWhitespace: true, removeComments: true, removeEmptyElements: true}))
+      .pipe(gulp.dest('./'));
+
+    gulp.src('dev/font-awesome/**')
+      .pipe(gulp.dest('build/font-awesome'));
+
+    gulp.src('dev/fonts/**')
+        .pipe(gulp.dest('build/fonts'));
+
+});
